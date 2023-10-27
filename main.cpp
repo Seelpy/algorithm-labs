da506bba42a75abe963ed9eadec70feb0e95ca2d
Seelpy <maks-veselov-2003@mail.ru>
2023-10-27 23:40:39 +0300

    create lab-3 deleted petals

--- main.cpp
+++ main.cpp
@@ -36,141 +36,77 @@ C++ 17
 #include <fstream>
 #include <set>
 
-int findMax(const std::vector<int>& numbers) {
-    int max = numbers[0];
-
-    for (int i = 1; i < numbers.size(); ++i) {
-        if (numbers[i] > max) {
-            max = numbers[i];
-        }
-    }
-
-    return max;
-}
-
-std::vector<int> findIds(const std::vector<int>& numbers, int max) {
-    std::vector<int> ids;
-
-    for (int i = 1; i <= numbers.size(); ++i) {
-        if (numbers[i] == max) {
-            ids.push_back(i);
-        }
-    }
-
-    return ids;
-}
-
-int findSecondMax(const std::vector<int>& numbers) {
-    int max = 0;
-    int second_max = 0;
-
-    for (int i = 1; i < numbers.size(); ++i) {
-        if (numbers[i] > max) {
-            second_max = max;
-            max = numbers[i];
-        } else if (numbers[i] > second_max && numbers[i] != max) {
-            second_max = numbers[i];
-        }
-    }
-
-    return second_max;
-}
-
-void dfs(int node, const std::vector<std::vector<int>>& adj_list, std::vector<bool>& visited, std::vector<int>& node_distances, int distance, int& farthest_node) {
-    visited[node] = true;
-
-    node_distances[node] = distance;
-
-    for (int neighbor : adj_list[node]) {
-        if (!visited[neighbor]) {
-            int new_distance = distance + 1;
-            int new_farthest_node = farthest_node;
-            dfs(neighbor, adj_list, visited, node_distances, new_distance, new_farthest_node);
-        }
-    }
-}
-
-int getFather(int node, const std::vector<std::vector<int>>& adj_list, std::vector<int> distances) {
-    int id;
-    for (int neighbor : adj_list[node]) {
-        if (distances[neighbor] < distances[node]) {
-            return neighbor;
-        }
-    }
-    std::cout<<"ERROR";
-    return -1;
-}
-
-std::vector<int> backwardDfs(int node, const std::vector<std::vector<int>>& adj_list, std::vector<int> distances) {
-    std::vector<int> track;
-    track.push_back(node);
-    while (distances[node] != 0) {
-        int father = getFather(node, adj_list, distances);
-        track.push_back(father);
-        node = father;
-    }
-
-    return track;
-}
-void print(std::vector<int> const &input) {
-    for (int i = 0; i < input.size(); i++) {
-        std::cout << input.at(i) << ' ';
+void print(std::vector<int>* const &input) {
+    for (int i = 0; i < input->size(); i++) {
+        std::cout << input->at(i) << ' ';
     }
     std::cout << std::endl;
 }
 
-std::vector<int> findPetals(const std::vector<std::vector<int>>& adj_list, std::vector<int> distances) {
+std::vector<int> deletedPetals(std::vector<std::vector<int>*>& adj_list, std::vector<int>& deleted_petals) {
     std::vector<int> petals;
-    petals.push_back(0);
-    for (int i = 1; i <= adj_list.size(); i ++){
-        if (adj_list[i].size() == 1) {
-            petals.push_back(distances[i]);
-        } else {
-            petals.push_back(0);
+    for (int i = 1; i < adj_list.size(); i ++){
+        if (deleted_petals[i] == 1) {
+            continue;
+        }
+        if (adj_list[i]->size() == 1) {
+            petals.push_back(i);
+            deleted_petals[i] = 1;
+            adj_list[i]->erase(adj_list[i]->begin());
+            for (int j = 1; j < adj_list.size(); j++){
+                int index = -1;
+                for (int k = 0; k < adj_list[j]->size(); k++) {
+                    if (adj_list.at(j)->at(k) == i) {
+                        index = k;
+                        break;
+                    }
+                }
+                std::vector<int>* a = adj_list[j];
+                if (index == -1) {
+                    continue;
+                }
+                a->erase(std::next(a->begin(), index));
+            }
         }
     }
+
     return petals;
 }
 
 
-
 std::vector<int> find_desired_computers(int N, const std::vector<std::pair<int, int>>& connections) {
-    std::vector<std::vector<int>> adj_list(N + 1);
+    std::vector<std::vector<int>*> adj_list;
+    for (int i = 0 ; i <= N; i++) {
+        std::vector<int>* m = new std::vector<int>;
+        adj_list.push_back(m);
+    }
 
     for (const auto& connection : connections) {
         int a = connection.first;
         int b = connection.second;
-        adj_list[a].push_back(b);
-        adj_list[b].push_back(a);
+        adj_list[a]->push_back(b);
+        adj_list[b]->push_back(a);
     }
 
-    std::vector<int> desired_computers;
+    std::vector<int> deleted_petals(N + 1, 0);
 
-    std::vector<bool> visited(N + 1, false);
-    std::vector<int> distances(N + 1, 0);
-    int distance = 0;
-    int farthest_node = 1;
-    dfs(farthest_node, adj_list, visited, distances, distance, farthest_node);
-    std::vector<int> petals = findPetals(adj_list, distances);
+    std::vector<int> petals = {};
 
-    int max = findMax(petals);
-    int second_max = findSecondMax(petals);
-    int dif = max - second_max;
-
-    std::vector<int> ids = findIds(petals, max);
-    for (const auto& node: ids) {
-        std::vector<int> track = backwardDfs(node, adj_list, distances);
-        if (dif % 2 == 0) {
-            desired_computers.push_back(track[track.size()  - 1 - dif/2]);
-        } else {
-            desired_computers.push_back(track[track.size()  - 1 - (dif + 1)/2]);
-            desired_computers.push_back(track[track.size()  - 1 - (dif - 1)/2]);
+    while (true) {
+        petals = deletedPetals(adj_list, deleted_petals);
+        if (petals.size() == 0) {
+            break;
         }
     }
 
-    std::set<int> s(desired_computers.begin(), desired_computers.end());
 
-    return std::vector<int>(s.begin(), s.end());
+    std::vector<int> result;
+    for (int i = 1; i < deleted_petals.size(); i++) {
+        if (deleted_petals[i] == 0) {
+            result.push_back(i);
+        }
+    }
+    return result;
 }
 
 int main() {
