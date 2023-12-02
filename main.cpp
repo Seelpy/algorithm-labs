a2fa6b45bb30ddc7a04d9b6004d0e46c702e7930
Seelpy <maks-veselov-2003@mail.ru>
2023-12-02 03:47:18 +0300

    Финал

--- main.cpp
+++ main.cpp
@@ -21,177 +21,52 @@ C++ 17
 typedef std::pair<int, int> pair;
 typedef std::pair<pair, int> connection;
 typedef std::vector<connection> list;
-typedef std::map<int, bool> used;
-typedef std::map<std::string, bool> build_list;
-typedef std::vector<std::vector<int>> adjacency;
-typedef std::map<int, std::map<int, int>> adjacency2;
 
-
-std::string generateKey(int a, int b) {
-    return std::to_string(a) + "-" + std::to_string(b);
+void addEdge(std::vector<pair> adj[], connection con) {
+    adj[con.first.first].emplace_back(con.first.second, con.second);
+    adj[con.first.second].emplace_back(con.first.first, con.second);
 }
 
-void printc(connection con) {
-    std::cout<<con.first.first << " " << con.first.second << " " << con.second << "\n";
-}
+list prim(std::vector<pair> adj[], int n) {
+    std::priority_queue<pair, std::vector<pair>, std::greater<pair> > pq;
 
-void print(list *l){
-    for (auto con: *l) {
-        printc(con);
-    }
-}
+    std::vector<int> key(n, INT_MAX);
+    std::vector<int> parent(n, -2);
+    std::vector<bool> useage(n, false);
+    int src = 0;
+    pq.emplace(0, src);
 
-//bool compare(connection a, connection b, build_list *build) {
-//    if ((*build)[generateKey(b)]) {
-//        return false;
-//    }
-//
-//    if ((*build)[generateKey(a)]) {
-//        return true;
-//    }
-//    return a.second < b.second;
-//}
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+        if (useage[u]) {
+            continue;
+        }
 
-void prepareUse(list *exist, list *miss, adjacency *adj, build_list *build) {
-    for (auto con: *miss) {
-        (*adj)[con.first.first][con.first.second] = con.second;
-        (*adj)[con.first.second][con.first.first] = con.second;
+        useage[u] = true;
+        for (auto x: adj[u]) {
+            int v = x.first;
+            int weight = x.second;
+            if (!useage[v] && key[v] > weight) {
+                key[v] = weight;
+                pq.emplace(key[v], v);
+                parent[v] = u;
+            }
+        }
     }
 
-    for (auto con: *exist) {
-        (*adj)[con.first.first][con.first.second] = 1;
-        (*adj)[con.first.second][con.first.first] = 1;
-        (*build)[generateKey(con.first.first, con.first.second)] = true;
-    }
-}
-
-
-
-list prim(list *exist, list *miss, int n) {
-    used use;
-    adjacency adj;
     list result;
-    build_list build;
 
-    adj.resize(n + 1, std::vector<int>(n + 1, 0));
-
-    for (int i = 0; i < n + 1; i++) {
-        adj.push_back(std::vector<int>(n + 1, 0));
-    }
-
-    prepareUse(exist, miss, &adj, &build);
-    use[1] = true;
-    int no_edge = 0;
-
-    while (no_edge < n - 1 + exist->size() ) {
-        int min = INT_MAX;
-        int x = 0;
-        int y = 0;
-
-        for (auto used_node: use) {
-            int i = used_node.first;
-            for (int j = 1; j < n + 1; j++) {
-                if ( adj[i][j]) { // not in selected and there is an edge
-                    if (!use[j]) {
-                        use.erase(j);
-                        if (min > adj[i][j] || build[generateKey(i, j)] || build[generateKey(j, i)]) {
-                            min = adj[i][j];
-                            x = i;
-                            y = j;
-                        }
-                    }
-                }
-            }
-        }
-        if (x == 0 && y == 0) {
-            break;
-        }
-
-        if (!build[generateKey(x, y)] && !build[generateKey(y, x)]) {
-            std::cout << x <<  " - " << y << " :  " << adj[x][y];
-            std::cout << std::endl;
-        } else {
-            std::cout << "- " << x <<  " - " << y << " :  " << adj[x][y];
-            std::cout << std::endl;
-        }
-        use[y] = true;
-        no_edge++;
-    }
-
-    return result;
-}
-
-void prepareUse2(list *exist, list *miss, adjacency2 *adj, build_list *build) {
-    for (auto con: *miss) {
-        (*adj)[con.first.first][con.first.second] = con.second;
-        (*adj)[con.first.second][con.first.first] = con.second;
-    }
-
-    for (auto con: *exist) {
-        (*adj)[con.first.first][con.first.second] = -1;
-        (*adj)[con.first.second][con.first.first] = -1;
-        (*build)[generateKey(con.first.first, con.first.second)] = true;
-    }
-}
-
-list prim2(list *exist, list *miss, int n) {
-    used use;
-    adjacency2 adj;
-    list result;
-    build_list build;
-
-    prepareUse2(exist, miss, &adj, &build);
-    use[1] = true;
-    int no_edge = 0;
-
-    while (no_edge < n - 1 + exist->size() ) {
-        int min = INT_MAX;
-        int x = 0;
-        int y = 0;
-
-        for (auto used_node: use) {
-            if (!used_node.second) {
-                continue;
-            }
-            int i = used_node.first;
-            for (auto node: adj[i]) {
-                int j = node.first;
-                if (!use[j]) {
-                    int w = adj[i][j];
-                    if (min > w) {
-                        min = w;
-                        x = i;
-                        y = j;
-                        if (w < 0) {
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        if (x == 0 && y == 0) {
-            int a = 1;
-            break;
-        }
-
-        int w = adj[x][y];
-        if (w > 0) {
-            connection con;
-            con.first.first = x;
-            con.first.second = y;
-            con.second = w;
+    for (int i = 1; i < n; ++i) {
+        connection con;
+        con.first.first = parent[i] + 1;
+        con.first.second = i + 1;
+        con.second = key[i];
+        if (con.second  > -1) {
             result.push_back(con);
-            adj[x].erase(y);
-        } else {
-            if (no_edge % 25 == 0) {
-                std::cout<<no_edge << "\n";
-            }
-            std::cout << "- " << x <<  " - " << y << " :  " << adj[x][y];
-            std::cout << std::endl;
         }
-        use[y] = true;
-        no_edge++;
     }
+
 
     return result;
 }
@@ -206,31 +81,37 @@ int main(int argc, char **argv) {
     int n, ex;
     in >> n >> ex;
 
+    std::vector<pair> adj[n];
+
     list exist_cons;
-    for (int i = 0; i < ex; i++){
-        connection conn;
-        in >> conn.first.first >> conn.first.second;
-        conn.second = 0;
-        exist_cons.push_back(conn);
+    for (int i = 0; i < ex; i++) {
+        connection con;
+        in >> con.first.first >> con.first.second;
+        con.first.first--;
+        con.first.second--;
+        con.second = -1;
+        addEdge(adj, con);
     }
     list cons;
     while (in.peek() != EOF && !in.eof()) {
-        connection conn;
-        in >> conn.first.first >> conn.first.second >> conn.second;
-        if (conn.first.first == 0) {
+        connection con;
+        in >> con.first.first >> con.first.second >> con.second;
+        con.first.first--;
+        con.first.second--;
+        if (con.first.first < 0) {
             break;
         }
-        cons.push_back(conn);
+        addEdge(adj, con);
     }
 
     list min_cons;
     int sum = 0;
-    min_cons = prim2(&exist_cons, &cons, n);
+    min_cons = prim(adj, n);
     out << "🛣️🚗Дороги: \n";
     for (auto con: min_cons) {
         sum += con.second;
         if (con.second > 0) {
-            out<<con.first.first << "-" << con.first.second << " " << con.second << "\n";
+            out << con.first.first << "-" << con.first.second << " " << con.second << "\n";
         }
     }
     out << "\n";
